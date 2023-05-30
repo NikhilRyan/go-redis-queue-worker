27b87bf58ead427d91c03453efda8c463726e5ca
Nikhil <nikhil.k@finbox.in>
2023-05-31 01:53:49 +0530

    queue stats

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 	"go-redis-queue/redisqueue"
@@ -27,6 +28,7 @@ func main() {
 
 	// Set up API routes
 	http.HandleFunc("/push", handlePushRequest)
+	http.HandleFunc("/queue-stats", handleQueueStatsRequest)
 
 	// Start the API server
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -63,14 +65,52 @@ func handlePushRequest(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func handleQueueStatsRequest(w http.ResponseWriter, r *http.Request) {
+	// Create a Redis client to get the queue stats
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	// Get the size of the queue
+	queueSize, err := client.LLen(context.Background(), "queue1").Result()
+	if err != nil {
+		http.Error(w, "Failed to get queue size", http.StatusInternalServerError)
+		return
+	}
+
+	// Get the queue stats
+	queueStats, err := client.LRange(context.Background(), "queue1", 0, -1).Result()
+	if err != nil {
+		http.Error(w, "Failed to get queue stats", http.StatusInternalServerError)
+		return
+	}
+
+	// Return the queue stats as the response
+	response := map[string]interface{}{
+		"queue": "queue1",
+		"size":  queueSize,
+		"stats": queueStats,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
 func workerFunction1(ctx context.Context, task string) {
 	// Implement the worker function for queue1
 	fmt.Printf("Worker1 processing task from queue1: %s\n", task)
 	// Perform the required task processing logic for queue1
+
+	// Simulate some work
+	time.Sleep(1 * time.Second)
 }
 
 func workerFunction2(ctx context.Context, task string) {
 	// Implement the worker function for queue2
 	fmt.Printf("Worker2 processing task from queue2: %s\n", task)
 	// Perform the required task processing logic for queue2
+
+	// Simulate some work
+	time.Sleep(1 * time.Second)
 }
